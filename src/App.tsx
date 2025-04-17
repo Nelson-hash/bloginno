0e74a8c7daf69741be62cabd28798c48bd170e2a
Nelson-hash <nlsn.remy@gmail.com>
2025-04-17 14:41:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
 import AdminDashboard from './pages/AdminDashboard';
 import ArticlePage from './pages/ArticlePage';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -13,7 +12,6 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/article/:id" element={<ArticlePage />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
       <Route 
         path="/admin" 
         element={
